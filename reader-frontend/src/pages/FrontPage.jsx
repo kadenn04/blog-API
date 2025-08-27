47bdd033bed24450febda510316165dbe6dcb2b8
kadenn04 <kadennguyen101@gmail.com>
2025-08-27 17:15:16 -0500

    Rewrote FrontPage to incorporate both notes and posts

--- reader-frontend/src/pages/FrontPage.jsx
+++ reader-frontend/src/pages/FrontPage.jsx
@@ -1,21 +1,68 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { Outlet } from "react-router-dom";
+import "./FrontPage.css"
 
 async function getAllPosts() {
     const response = await fetch("http://localhost:3001/posts", {mode: 'cors'});
     const posts = await response.json();
+    posts.map((post) => {
+        post.type = "post";
+    })
     return posts;
 }
 
+async function getAllNotes() {
+    const response = await fetch("http://localhost:3001/notes", {mode: 'cors'});
+    const notes = await response.json();
+    notes.map((note) => {
+        note.type = "note";
+    })
+    return notes;
+}
+
+async function getContent() {
+    const posts = await getAllPosts();
+    const notes = await getAllNotes();
+    let content = posts.concat(notes);
+    return content;
+
+}
+
+function NotePreview(props) {
+    return(
+        <div className="note-preview">
+            <h1>This is a note</h1>
+            {props.note.content}
+        </div>
+
+    )
+}
+
+function PostPreview(props) {
+    console.log(props);
+    return(
+        <div className="post-preview">
+            <h1>This is a post</h1>
+            {props.post.content}
+        </div>
+
+    )
+}
+
+
+function sortByUpdatedAt(content) {
+    return content.sort((a,b) => {
+        return new Date(b.updatedAt) - new Date(a.updatedAt)
+    })
+}
+
 function FrontPage() {
-    const [posts, setPosts]  = useState(null);
+    const [content, setContent]  = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        getAllPosts()
-        .then(result => { setPosts(result); })
+        getContent()
+        .then(result => { setContent(sortByUpdatedAt(result)); })
         .catch(err => setError(err))
         .finally(() => setLoading(false));
         
@@ -26,18 +73,17 @@ function FrontPage() {
 
 
     return (
-        <>
-            <h1>Posts: </h1>
-            <ul> 
-                {posts.map((post)=> {
-                    return <li key={post.id}>
-                        {post.title} 
-                        <Link to ={"post/"+post.id}> Go </Link>
-                        </li>
+         
+            <div id="page-content">
+                {content.map((con) => {
+                    if (con.type == "note") {
+                        return (<NotePreview note={con}/>)
+                    } else if (con.type == "post") {
+                        return (<PostPreview post={con}/>)
+                    }
                 })}
-            </ul>
-            <Outlet />
-        </>
+            </div>
+        
     )
 }
 
